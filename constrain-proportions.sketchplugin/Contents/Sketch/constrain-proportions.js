ebd65d76f60699262e9bc34043584c565ff1015f
Shane Goodwin <ithacanz@gmail.com>
2017-01-10 09:48:09 +1300

    initial working version

--- constrain-proportions.sketchplugin/Contents/Sketch/constrain-proportions.js
+++ constrain-proportions.sketchplugin/Contents/Sketch/constrain-proportions.js
@@ -2,49 +2,63 @@
 
 function onRun(context) {
 
-  // We are passed a context variable when we're run.
-  // We use this to get hold of a javascript object
-  // which we can use in turn to manipulate Sketch.
-  var sketch = context.api()
+    // We are passed a context variable when we're run.
+    // We use this to get hold of a javascript object
+    // which we can use in turn to manipulate Sketch.
+    var sketch = context.api()
 
-  // Next we want to extract the selected page of the selected (front-most) document
-  var document = sketch.selectedDocument // don't actually need these because this is so simple
-  var page = document.selectedPage // don't actually need these because this is so simple
+    // Next we want to extract the selected page of the selected (front-most) document
+    var document = sketch.selectedDocument // don't actually need these because this is so simple
+    var page = document.selectedPage // don't actually need these because this is so simple
 
-  var selectedLayers = context.selection;
-  var selectedCount = selectedLayers.count();
-  log('--- constrain proportions ---')
-  log('Selected layers:');
+    var selectedLayers = context.selection;
+    var selectedCount = selectedLayers.count();
 
+    log('--- constrain proportions ---')
 
-  if (selectedCount == 0) {
-    // nothing to do, silently do it
-    log('  No layers selected.');
-  } else {
-    if (selectedCount == 1) {
-      // if we have just a single object selected, then we just want to toggle constrainProportions
-      var layer = selectedLayers[0];
-      var f = [layer frame];
+    if (selectedCount == 0) {
+        // nothing to do, silently do it
+        log('  No layers selected.');
+    } else if (selectedCount == 1) {
+        // if we have just a single object selected, then we just want to toggle constrainProportions
+        var layer = selectedLayers[0];
+        var f = [layer frame];
 
-      if ([f constrainProportions]) {
-        [layer setConstrainProportions:0]
-      } else {
-        [layer setConstrainProportions:1]
-      }
-      log('  '+layer.name() + ': ' + [f constrainProportions])
+        if ([f constrainProportions]) {
+            [layer setConstrainProportions: 0]
+        } else {
+            [layer setConstrainProportions: 1]
+        }
+        log('  1 layer: ' + layer.name() + ': ' + [f constrainProportions]);
 
     } else {
-      // multiple objects selected, so we check their constrainProportions state.
-      // If it's different, we constrain all of them. If it's not different, then we toggle all of them.
+        // multiple objects selected
 
-      for (var i = 0; i < selectedCount; i++) {
-        var layer = selectedLayers[i];
-        var f = [layer frame];
-        
-        log('  '+layer.name() + ': ' + [f constrainProportions])
+        log('  ' + selectedCount + ' layers:')
+        var cpSum = 0;
 
-        [layer setConstrainProportions:1];
-      };
+        for (var i = 0; i < selectedCount; i++) {
+            var layer = selectedLayers[i];
+            var f = [layer frame];
+
+            cpSum = cpSum + [f constrainProportions];
+            log('    ' + layer.name() + ': ' + [f constrainProportions])
+        };
+
+        log('  Sum: ' + cpSum);
+
+        // If the constraints are different or they're all unconstrained, we
+        // constrain them all. If they're all constrained, we unconstrain them all
+        var newConstraint = 1;
+        if (cpSum == selectedCount)
+            newConstraint = 0;
+
+        log('  New constraint: ' + newConstraint);
+
+        // write the new constraint into all objects
+        for (var i = 0; i < selectedCount; i++) {
+            var layer = selectedLayers[i];
+            [layer setConstrainProportions: newConstraint];
+        };
     };
-  };
 };
