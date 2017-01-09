2f6ca720ac11d69872e856ac743abcc8f7092622
Shane Goodwin <ithacanz@gmail.com>
2017-01-10 08:44:33 +1300

    initial commit

---
+++ constrain-proportions.sketchplugin/Contents/Sketch/constrain-proportions.js
@@ -0,0 +1,50 @@
+// constrainProportions
+
+function onRun(context) {
+
+  // We are passed a context variable when we're run.
+  // We use this to get hold of a javascript object
+  // which we can use in turn to manipulate Sketch.
+  var sketch = context.api()
+
+  // Next we want to extract the selected page of the selected (front-most) document
+  var document = sketch.selectedDocument // don't actually need these because this is so simple
+  var page = document.selectedPage // don't actually need these because this is so simple
+
+  var selectedLayers = context.selection;
+  var selectedCount = selectedLayers.count();
+  log('--- constrain proportions ---')
+  log('Selected layers:');
+
+
+  if (selectedCount == 0) {
+    // nothing to do, silently do it
+    log('  No layers selected.');
+  } else {
+    if (selectedCount == 1) {
+      // if we have just a single object selected, then we just want to toggle constrainProportions
+      var layer = selectedLayers[0];
+      var f = [layer frame];
+
+      if ([f constrainProportions]) {
+        [layer setConstrainProportions:0]
+      } else {
+        [layer setConstrainProportions:1]
+      }
+      log('  '+layer.name() + ': ' + [f constrainProportions])
+
+    } else {
+      // multiple objects selected, so we check their constrainProportions state.
+      // If it's different, we constrain all of them. If it's not different, then we toggle all of them.
+
+      for (var i = 0; i < selectedCount; i++) {
+        var layer = selectedLayers[i];
+        var f = [layer frame];
+        
+        log('  '+layer.name() + ': ' + [f constrainProportions])
+
+        [layer setConstrainProportions:1];
+      };
+    };
+  };
+};
